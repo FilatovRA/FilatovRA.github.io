5dd81460d71c0c98e350e3149ba52248991b4a31
FilatovRA <filromashka@gmail.com>
2017-11-09 16:08:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(function(){
 function launchTyping(){
     var options = {
-        strings: ["Hello, my name is Roman,"],
-        typeSpeed: 40
+        strings: ["Hello, my name is Roman,\nthis is my personal page."],
+        typeSpeed: 40,
+         startDelay: 3000,
       }
       var typed = new Typed(".selfTypingText", options);
 }

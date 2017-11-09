64f1a6834a376e2cf65c44cbcf926fd59366621e
FilatovRA <filromashka@gmail.com>
2017-11-09 15:22:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+function launchTyping() {
+    var options = {
+        strings: ["Hello, my name is Roman,"],
+        typeSpeed: 40
+      }
+      var typed = new Typed(".selfTypingText", options);
+}
+})
